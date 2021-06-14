571e052da1960ee5d8403643190619bd2d8e6b01
dhreyes <2590@holbertonschool.com>
2021-06-14 18:57:12 +0000

    Answer task 4, and update header

---
+++ 100-shell_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * shell_sort - sort data of array using shell sort algo
+ * @array: array to be sorted
+ * @size: size of array
+ * Return: none
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, n, i;
+	int number;
+
+	while (gap < size / 3)
+		gap = 3 * gap + 1;
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			for (n = i; n > 0 && array[n] < array[n - gap]; n -= gap)
+			{
+				number = array[n];
+				array[n] = array[n - gap];
+				array[n - gap] = number;
+			}
+		}
+		gap /= 3;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -22,5 +22,6 @@ void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
