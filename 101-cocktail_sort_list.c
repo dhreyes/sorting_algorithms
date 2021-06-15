c35aca5fa90ace978291541b348b610b3cbaaefd
roquesrolando <roquesrolando@hotmail.com>
2021-06-15 17:18:38 +0000

    fix output

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -28,21 +28,21 @@ void cocktail_sort_list(listint_t **list)
 			if (*list == temp)
 				*list = iterator;
 			print_list(*list);
-			while (iterator2 && flag == 1)
+		}
+		while (iterator2 && flag == 1)
+		{
+			if (iterator2->prev == NULL)
+				flag = 0;
+			if (temp->n < iterator2->n)
 			{
-				if (iterator2->prev == NULL)
-					flag = 0;
-				if (temp->n < iterator2->n)
-				{
-					iterator = iterator2;
-					node_swap(iterator2, temp);
-					if (*list == iterator2)
-						*list = temp;
-					print_list(*list);
-				}
-				temp = iterator2;
-				iterator2 = temp->prev;
+				iterator = iterator2;
+				node_swap(iterator2, temp);
+				if (*list == iterator2)
+					*list = temp;
+				print_list(*list);
 			}
+			temp = iterator2;
+			iterator2 = temp->prev;
 		}
 		temp = iterator;
 		iterator = iterator->next;
